a83fcf83a32cd776ee61b30207a2d9a083f14610
Verona4321 <sverona432@yandex.ru>
2024-08-31 04:22:51 +0400

    a lot of fixes

--- send.php
+++ send.php
@@ -9,7 +9,7 @@ $email = urldecode($email);
 $name = trim($name);
 $email = trim($email);
 if (mail("sverona432@yandex.ru", "Заявка с сайта",  "Имя: " .$name.",  E-mail: " .$email.  ",  Сообщение: ".$text.""))
- {     echo "Спасибо, Ваше сообщение успешно отправлено!";
+{     echo "Спасибо, Ваше сообщение успешно отправлено!";
 } else {
     echo "При отправке сообщения возникли ошибки :(";
 }?>
