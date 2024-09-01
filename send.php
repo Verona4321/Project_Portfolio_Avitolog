705a82ba0b6334a1973026fbfdab3f0a391303a6
Verona4321 <sverona432@yandex.ru>
2024-09-02 01:58:15 +0400

    icons

--- send.php
+++ send.php
@@ -8,7 +8,7 @@ $name = urldecode($name);
 $email = urldecode($email);
 $name = trim($name);
 $email = trim($email);
-if (mail("sverona432@yandex.ru", "Заявка с сайта",  "Имя: " .$name.",  E-mail: " .$email.  ",  Сообщение: ".$text.""))
+if (mail("lysovv.a2002@gmail.com", "Заявка с сайта",  "Имя: " .$name.",  E-mail: " .$email.  ",  Сообщение: ".$text.""))
 {     echo "Спасибо, Ваше сообщение успешно отправлено!" ;
 } else {
     echo "При отправке сообщения возникли ошибки :(";
